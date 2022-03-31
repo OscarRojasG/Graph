2fac3ece3a2d5d126bb67a20463d0d3f175f09c1
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2022-03-31 16:03:00 -0300

    Dibujando triángulo en pantalla

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void processInput(GLFWwindow *window); 
+void drawTriangle(GLFWwindow *window);
+
+unsigned int createVertexBufferObject();
+unsigned int createVertexArrayObject();
+unsigned int createVertexShader();
+unsigned int createFragmentShader();
+unsigned int createShaderProgram();
+void checkShaderCompileErrors(unsigned int shader);
+void checkShaderProgramErrors(unsigned int program);
 
 int main()
 {
@@ -32,8 +44,39 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Inicialización de objetos
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f,
+    }; 
+
+    unsigned int VBO = createVertexBufferObject();
+    unsigned int VAO = createVertexArrayObject();
+    unsigned int shaderProgram = createShaderProgram();
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0); 
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+ 
     while(!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
@@ -45,4 +88,102 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-} 
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+unsigned int createVertexBufferObject()
+{
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    return VBO;
+}
+
+unsigned int createVertexArrayObject()
+{
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    return VAO;
+}
+
+unsigned int createShaderProgram()
+{ 
+    unsigned int vertexShader = createVertexShader();
+    unsigned int fragmentShader = createFragmentShader();
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkShaderProgramErrors(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderProgram;
+}
+
+unsigned int createVertexShader()
+{
+    const char *vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompileErrors(vertexShader);
+
+    return vertexShader;
+}
+
+unsigned int createFragmentShader()
+{
+    const char *fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompileErrors(fragmentShader);
+
+    return fragmentShader;
+}
+
+void checkShaderCompileErrors(unsigned int shader)
+{
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("Error al compilar el shader: %s", infoLog);
+    }
+}
+
+void checkShaderProgramErrors(unsigned int program)
+{
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+    if(!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        printf("Error al enlazar los shaders: %s", infoLog);
+    }
+}
