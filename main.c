52d6546ca8cbf72c1e0c835083386061ab3848b4
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2022-03-31 18:47:56 -0300

    Triángulo multicolor

--- main.c
+++ main.c
@@ -49,14 +49,12 @@ int main()
     // Inicialización de objetos
 
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+         0.0f,  0.5f,  0.0f,  1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.0f,  0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.0f,  0.0f, 0.0f, 1.0f,
     }; 
     unsigned int indices[] = {
-        0, 1, 3, // Primer triángulo
-        1, 2, 3  // Segundo triángulo
+        0, 1, 2
     }; 
 
     unsigned int VBO = createVertexBufferObject();
@@ -72,8 +70,13 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Posición
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0); 
+
+    // Color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1); 
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -88,13 +91,6 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-
-        float timeValue = glfwGetTime();
-        float redValue = sin(timeValue) / 2.0f + 0.5f;
-        float greenValue = cos(timeValue) / 2.0f + 0.5f;
-        float blueValue = sin(timeValue*-1) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "vertexColor");
-        glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -161,9 +157,12 @@ unsigned int createVertexShader()
     const char *vertexShaderSource =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 vertexColor;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   vertexColor = aColor;\n"
         "}\0";
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -178,11 +177,11 @@ unsigned int createFragmentShader()
 {
     const char *fragmentShaderSource =
         "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "uniform vec4 vertexColor;\n"
+        "out vec4 fragmentColor;\n"
+        "in vec3 vertexColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vertexColor;\n"
+        "   fragmentColor = vec4(vertexColor, 1.0);\n"
         "}\0";
 
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
